cf7bb944f6e32c1c29d03fe521857c3a61ba8103
osr-corgan <osr-corgan@eqdkp-plus.eu>
2008-07-28 22:30:30 +0000

    Moduls: Add CCPL License to all Fileheader use only one headerstyle | set SVN Keywords

--- whoisonline/language/english.php
+++ whoisonline/language/english.php
@@ -1,11 +1,20 @@
 <?php
-/******************************
- * EQDKP PLUS
- * Who is online
- * (c) 2008 by Aderyn
- * ------------------
+/*
+ * Project:     EQdkp-Plus
+ * License:     Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:       2008
+ * Date:        $Date: $
+ * -----------------------------------------------------------------------
+ * @author      $Author:  $
+ * @copyright   (c) 2008 by Aderyn
+ * @link        http://eqdkp-plus.com
+ * @package     eqdkp-plus
+ * @version     $Rev:  $
+ * 
  * $Id: $
- ******************************/
+ */
 
 if ( !defined('EQDKP_INC') ){
     header('HTTP/1.0 404 Not Found');exit;
