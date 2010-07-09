9365add6e42ac5080ef9da59a5865ea498dc4b1b
corgan <corgan@eqdkp-plus.eu>
2010-07-09 16:07:05 +0000

    french & spanish translation

--- language/french.php
+++ language/french.php
@@ -1,37 +1,21 @@
 <?php
-/*
- * Project:     EQdkp-Plus
- * License:     Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
- * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
- * -----------------------------------------------------------------------
- * Began:       2008
- * Date:        $Date: $
- * -----------------------------------------------------------------------
- * @author      $Author:  $
- * @copyright   (c) 2008 by Aderyn
- * @link        http://eqdkp-plus.com
- * @package     eqdkp-plus
- * @version     $Rev:  $
- * 
- * $Id: $
- */
 
-if ( !defined('EQDKP_INC') ){
-    header('HTTP/1.0 404 Not Found');exit;
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
 }
 
-$plang = array_merge($plang, array(
-  // Title
-  'whoisonline'                => 'Utilisateurs connectés',
-  
-  //  Settings
-  'wo_limit'                   => 'Limite max des utilisateurs.',
-  'wo_dontshowoffline'         => 'Masquer les utilisateurs déconnectés.',
-   
-  // Portal Modul
-  'wo_online'                  => 'Utilisateur connecté',
-  'wo_last_online'             => 'Dernière connexion : ',
-  'wo_date_format'             => 'd. M Y H:i:s',  // DD. MMM YYYY HH:mm:ss
-));
+//Language: French	
+//Created by EQdkp Plus Translation Tool on  2010-07-09 13:55
+//File: module_whoisonline
+//Source-Language: english
 
+$alang = array( 
+"whoisonline" => "Utilisateurs connectés",
+"wo_limit" => "Limite max des utilisateurs.",
+"wo_dontshowoffline" => "Masquer les utilisateurs déconnectés.",
+"wo_online" => "Utilisateur connecté",
+"wo_last_online" => "Dernière connexion : ",
+"wo_date_format" => "d. M Y H:i:s",
+ );
+$plang = array_merge($plang, $alang);
 ?>
---
+++ language/spanish.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
+}
+
+//Language: Spanish	
+//Created by EQdkp Plus Translation Tool on  2010-07-09 15:10
+//File: module_whoisonline
+//Source-Language: english
+
+$alang = array( 
+"whoisonline" => "Usuarios conectados",
+"wo_limit" => "Máximo número de usuarios.",
+"wo_dontshowoffline" => "No mostrar usuarios desconectados.",
+"wo_online" => "Usuarios conectados",
+"wo_last_online" => "Última conexión:",
+"wo_date_format" => "d. M Y H:i:s",
+ );
+$plang = array_merge($plang, $alang);
+?>
